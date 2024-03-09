ab05330a9f22f3499cfe0156c3fbbf89e8c8e2a6
Nigel <mail@nigelmoes.nl>
2024-03-08 19:26:43 -0500

    Added setter injection example

--- src/main/java/com/nigel/SpringCoreDemo/rest/DemoController.java
+++ src/main/java/com/nigel/SpringCoreDemo/rest/DemoController.java
@@ -11,9 +11,16 @@ public class DemoController {
     // define a private field for the dependency
     private Coach myCoach;
 
+    /*
     // define a constructor for dependency injection
     @Autowired
     public DemoController(Coach theCoach) {
+        myCoach = theCoach;
+    }
+    */
+
+    @Autowired
+    public void setCoach(Coach theCoach) {
         myCoach = theCoach;
     }
 
