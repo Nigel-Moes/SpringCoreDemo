ebcad1df816ec332b6dfd537072b53cd3016bfdc
Nigel <mail@nigelmoes.nl>
2024-03-10 11:27:22 -0500

    Added lazy initialization of the beans

--- src/main/java/com/nigel/SpringCoreDemo/common/TrackCoach.java
+++ src/main/java/com/nigel/SpringCoreDemo/common/TrackCoach.java
@@ -1,11 +1,17 @@
 package com.nigel.SpringCoreDemo.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
+// @Primary
+// @Lazy
 public class TrackCoach implements Coach {
+
+    public TrackCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
     @Override
     public String getDailyWorkout() {
         return "Run a 5k";
--- src/main/java/com/nigel/SpringCoreDemo/rest/DemoController.java
+++ src/main/java/com/nigel/SpringCoreDemo/rest/DemoController.java
@@ -14,16 +14,11 @@ public class DemoController {
 
     // define a constructor for dependency injection
     @Autowired
-    public DemoController(Coach theCoach) {
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+        System.out.println("In constructor: " + getClass().getSimpleName());
         myCoach = theCoach;
     }
 
-    /*
-        @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
-        myCoach = theCoach;
-    }
-    */
 
     /*
     @Autowired
