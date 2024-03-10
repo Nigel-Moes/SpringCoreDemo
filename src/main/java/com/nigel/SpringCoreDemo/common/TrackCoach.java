e62e33050146821051f460874edc3d66942d752d
Nigel <mail@nigelmoes.nl>
2024-03-10 10:39:31 -0500

    Added a qualifier to the demo controller

--- src/main/java/com/nigel/SpringCoreDemo/common/TrackCoach.java
+++ src/main/java/com/nigel/SpringCoreDemo/common/TrackCoach.java
@@ -3,9 +3,9 @@ package com.nigel.SpringCoreDemo.common;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CricketCoach implements Coach {
+public class TrackCoach implements Coach {
     @Override
     public String getDailyWorkout() {
-        return "Practice fast bowling for 15 minutes";
+        return "Run a 5k";
     }
 }
--- src/main/java/com/nigel/SpringCoreDemo/rest/DemoController.java
+++ src/main/java/com/nigel/SpringCoreDemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.nigel.SpringCoreDemo.rest;
 
 import com.nigel.SpringCoreDemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,18 +12,18 @@ public class DemoController {
     // define a private field for the dependency
     private Coach myCoach;
 
-    /*
     // define a constructor for dependency injection
     @Autowired
-    public DemoController(Coach theCoach) {
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
         myCoach = theCoach;
     }
-    */
 
+    /*
     @Autowired
     public void setCoach(Coach theCoach) {
         myCoach = theCoach;
     }
+    */
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout(){
